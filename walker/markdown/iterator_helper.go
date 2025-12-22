7cd149ba637b1c73308a6051a67ac882cb68dac1
Théo Bori <theobori@disroot.org>
2025-12-22 20:28:35 +0100

    :zap: Optimized the Markdown walker iterator helper

--- walker/markdown/iterator_helper.go
+++ walker/markdown/iterator_helper.go
@@ -1,11 +1,29 @@
 package markdown
 
 import (
+	"strings"
+
 	"github.com/yuin/goldmark/ast"
 )
 
 func (w *Walker) walkIteratorHelper(node ast.Node) (string, error) {
-	s := ""
+	// a good optimization here is to use a string builder when iterating instead
+	// of an immutable string
+	//
+	// With an immutable string on a Markdown file with >60k lines:
+	//________________________________________________________
+	// Executed in    4.52 secs    fish           external
+	// usr time   18.02 secs    0.32 millis   18.02 secs
+	// sys time    1.03 secs    1.77 millis    1.03 secs
+	//
+	// With a string builder
+	//
+	// ________________________________________________________
+	// Executed in  197.16 millis    fish           external
+	// usr time  311.73 millis    1.13 millis  310.60 millis
+	// sys time   57.08 millis    1.02 millis   56.06 millis
+	//
+	s := strings.Builder{}
 
 	for c := node.FirstChild(); c != nil; c = c.NextSibling() {
 		res, err := w.Walk(c)
@@ -13,8 +31,11 @@ func (w *Walker) walkIteratorHelper(node ast.Node) (string, error) {
 			return "", err
 		}
 
-		s += res
+		_, err = s.WriteString(res)
+		if err != nil {
+			return "", err
+		}
 	}
 
-	return s, nil
+	return s.String(), nil
 }
